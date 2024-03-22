d357cdc63ca5531eb237f77ffe9de1de7849939b
NadiaSabaEY <nadia.saba@ey.com>
2024-03-21 23:38:56 -0500

    Removed class

--- src/main/java/com/investmentapplication/investmentapplication/services/implementation/LoginService.java
+++
@@ -1,8 +0,0 @@
-package com.investmentapplication.investmentapplication.services.implementation;
-
-public interface LoginService {
-        boolean login(String userName, String password);
-        boolean register(String userName, String password);
-    }
-
-
