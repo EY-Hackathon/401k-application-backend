ae04ac2722e62391a9837d7972be45ff06a9eb06
vidisha566 <vidishashetty01@gmail.com>
2024-03-22 19:02:30 -0500

    Validation updates

--- src/main/java/com/investmentapplication/investmentapplication/controller/UserSignupController.java
+++ src/main/java/com/investmentapplication/investmentapplication/controller/UserSignupController.java
@@ -2,47 +2,55 @@ package com.investmentapplication.investmentapplication.controller;
 
 import com.investmentapplication.investmentapplication.dto.UserSignUpDTO;
 import com.investmentapplication.investmentapplication.services.UserSignUpServices;
+import com.investmentapplication.investmentapplication.entity.TokenResponse;
+import com.investmentapplication.investmentapplication.exception.UserAlreadyExistsException;
+import com.investmentapplication.investmentapplication.util.ErrorCode;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.FieldError;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
-/**
- * Controller class for handling user sign-up related API endpoints.
- */
 @RestController
 @Validated
 @CrossOrigin
 @RequestMapping("/api")
-public class UserSignupController {
+public class UserSignupController implements UserSignUpOperations {
 
     @Autowired
     private UserSignUpServices userSignUpServices;
 
-    public ResponseEntity<String> signup(@RequestBody @Validated UserSignUpDTO userSignUpDTO, BindingResult bindingResult) throws Exception {
-
-        if (bindingResult.hasErrors()) {
-            // Check for validation errors and return appropriate response
-            List<String> errorMessages = bindingResult.getFieldErrors().stream()
-                    .map(FieldError::getDefaultMessage) // Get the error message defined in the annotation
-                    .collect(Collectors.toList());
-
-            if (!errorMessages.isEmpty()) {
-                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(String.join(", ", errorMessages));
+    @PostMapping("/signup")
+    public ResponseEntity<TokenResponse> signup(@RequestBody @Validated UserSignUpDTO userSignUpDTO, BindingResult bindingResult) {
+        TokenResponse response = new TokenResponse(); // Create a new token response
+        try {
+            if (bindingResult.hasErrors()) {
+                // Check for validation errors and return appropriate response
+                List<String> errorMessages = bindingResult.getFieldErrors().stream()
+                        .map(FieldError::getDefaultMessage) // Get the error message defined in the annotation
+                        .collect(Collectors.toList());
+                String responseMessage = String.join(",", errorMessages);
+                response.setMessage(responseMessage);
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
             }
+            else {
+
+                // Call the service method to add user
+                TokenResponse tokenResponse =userSignUpServices.addUser(userSignUpDTO);
+                response.setMessage(tokenResponse.getMessage());
+            }
+        } catch (UserAlreadyExistsException e) {
+            // Handle user already exists exception
+            response.setErrorCode(ErrorCode.USER_ALREADY_EXISTS);
+            response.setMessage(e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
         }
-        // Check if the email already exists
-        if (userSignUpServices.isEmailExists(userSignUpDTO.getEmail())) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Email already exists");
-        }
-        // Add the user if email doesn't exist and validation passes
-        userSignUpServices.addUser(userSignUpDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).body("User registered successfully");
+        // Return response with OK status
+        return new ResponseEntity<TokenResponse>(response, HttpStatus.OK);
     }
 }
